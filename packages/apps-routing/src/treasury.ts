b9c0bccfba06144fcf738d6a51d9b95ad5b0d9ab
VladStarostenko <vlad.starostenko.0501@gmail.com>
2020-10-20 10:40:23 +0300

    Change needs Api

--- packages/apps-routing/src/treasury.ts
+++ packages/apps-routing/src/treasury.ts
@@ -11,7 +11,7 @@ export default function create (t: TFunction): Route {
     Component,
     display: {
       needsApi: [
-        'tx.treasury.proposeSpend'
+        'tx.treasury.proposeBounty'
       ]
     },
     group: 'governance',
